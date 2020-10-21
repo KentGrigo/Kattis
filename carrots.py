ca5b5cb1a9a84239fb84680bbe4d7f35d48b05ed
Kent Grigo <KentGrigo@hotmail.com>
2020-10-21 10:35:36 +0200

    Solve carrots

---
+++ carrots.py
@@ -0,0 +1,8 @@
+data = input().split()
+numberOfParticipants = int(data[0])
+numberOfProblemsSolved = int(data[1])
+
+for i in range(numberOfParticipants):
+    input()
+
+print(numberOfProblemsSolved)
