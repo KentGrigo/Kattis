c5a18d5d4fe25240722f943674750e2e197d41fa
Kent Grigo <KentGrigo@hotmail.com>
2020-11-12 13:08:47 +0100

    Solve bijele

---
+++ bijele.py
@@ -0,0 +1,6 @@
+actualPieces = list(map(lambda piece: int(piece), input().split()))
+expectedPieces = [1, 1, 2, 2, 2, 8]
+
+difference = [str(expectedPiece - actualPiece) for (expectedPiece, actualPiece) in zip(expectedPieces, actualPieces)]
+output = " ".join(difference)
+print(output)
