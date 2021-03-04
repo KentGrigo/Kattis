3139eedfdd600a878fabdeb826b8f5c11c6721a7
Kent Grigo <KentGrigo@hotmail.com>
2021-03-04 22:51:16 +0100

    Solve speeding

---
+++ speeding.py
@@ -0,0 +1,15 @@
+import math
+
+numberOfPhotos = int(input())
+prevTime, prevDistance = list(map(int, input().split()))
+maxSpeed = 0
+for _ in range(numberOfPhotos - 1):
+    currTime, currDistance = list(map(int, input().split()))
+    diffTime = currTime - prevTime
+    diffDistance = currDistance - prevDistance
+    speed = diffDistance / diffTime
+    maxSpeed = max(speed, maxSpeed)
+
+    prevTime, prevDistance = currTime, currDistance
+
+print(math.floor(maxSpeed))
