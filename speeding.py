7b6dfede1bedd803fad176805521235eb63530b9
Kent Grigo <KentGrigo@hotmail.com>
2021-03-06 16:14:08 +0100

    Simplify speeding

--- speeding.py
+++ speeding.py
@@ -1,5 +1,3 @@
-import math
-
 numberOfPhotos = int(input())
 prevTime, prevDistance = list(map(int, input().split()))
 maxSpeed = 0
@@ -7,9 +5,10 @@ for _ in range(numberOfPhotos - 1):
     currTime, currDistance = list(map(int, input().split()))
     diffTime = currTime - prevTime
     diffDistance = currDistance - prevDistance
-    speed = diffDistance / diffTime
+    speed = diffDistance // diffTime
     maxSpeed = max(speed, maxSpeed)
 
-    prevTime, prevDistance = currTime, currDistance
+    prevTime = currTime
+    prevDistance = currDistance
 
-print(math.floor(maxSpeed))
+print(maxSpeed)
