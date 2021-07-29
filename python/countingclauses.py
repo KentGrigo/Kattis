3387ff466320d91007da6aa145d363a10599e897
Kent Grigo <KentGrigo@hotmail.com>
2021-07-29 19:53:54 +0200

    Solve countingclauses

---
+++ python/countingclauses.py
@@ -0,0 +1,8 @@
+numberOfClauses, _ = list(map(int, input().split()))
+for _ in range(numberOfClauses):
+    input()
+
+if 8 <= numberOfClauses:
+    print("satisfactory")
+else:
+    print("unsatisfactory")
