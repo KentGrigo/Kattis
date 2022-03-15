1c7c452ae5e057c51dd0ce9268ed497d6105ae70
Kent Grigo <KentGrigo@hotmail.com>
2022-03-15 23:11:52 +0100

    Solve betting

---
+++ python/betting.py
@@ -0,0 +1,6 @@
+betPercentageOption1 = int(input())
+betPercentageOption2 = (100 - betPercentageOption1)
+payoutRatioOption1 = 100 / betPercentageOption1
+payoutRatioOption2 = 100 / betPercentageOption2
+print(payoutRatioOption1)
+print(payoutRatioOption2)
