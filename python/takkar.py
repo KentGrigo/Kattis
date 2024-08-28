b1f9feb92415222c6679e2f7e0862774a130813c
Kent Grigo <KentGrigo@hotmail.com>
2024-08-28 13:09:02 +0200

    Solve takkar

---
+++ python/takkar.py
@@ -0,0 +1,9 @@
+sizeOfTrumpsButton = int(input())
+sizeOfKimJongUnsButton = int(input())
+
+if sizeOfKimJongUnsButton < sizeOfTrumpsButton:
+    print("MAGA!")
+elif sizeOfKimJongUnsButton == sizeOfTrumpsButton:
+    print("WORLD WAR 3!")
+else:
+    print("FAKE NEWS!")
