b5187c3399434b178a99fa98b890be15916abddd
Kent Grigo <KentGrigo@hotmail.com>
2026-06-02 13:19:43 +0200

    Solve antipalindrome

---
+++ python/antipalindrome.py
@@ -0,0 +1,29 @@
+def checkPart(text, leftIndex, rightIndex):
+    width = rightIndex - leftIndex
+    for offset in range(width):
+        leftCharacter = text[leftIndex + offset]
+        rightCharacter = text[rightIndex - offset]
+        if leftCharacter != rightCharacter:
+            return False
+        
+    return True
+
+def check(text):
+    for leftIndex in range(textLength):
+        for rightIndex in range(leftIndex + 1, textLength):
+            result = checkPart(text, leftIndex, rightIndex)
+            if result:
+                return True
+    
+    return False
+
+
+text = input().replace(' ', '').lower()
+textLength = len(text)
+isPalindrome = check(text)
+if isPalindrome:
+    print("Palindrome")
+else:
+    print("Anti-palindrome")
+
+            
