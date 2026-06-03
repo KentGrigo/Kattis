fd2148ba225b0cbb85c329fab0e4194b599d1728
Kent Grigo <KentGrigo@hotmail.com>
2026-06-03 12:39:54 +0200

    Simplify solution by using regex instead of FSM

--- python/parsinghex.py
+++ python/parsinghex.py
@@ -1,28 +1,9 @@
 import sys
+import re
 
-INITIAL = 'INITIAL'
-FOUND_0 = 'FOUND_0'
-FOUND_X = 'FOUND_X'
-FOUND_NUMBER = 'FOUND_NUMBER'
+hexPattern = re.compile(r'0[xX][0-9a-fA-F]{1,8}')
 
 for line in sys.stdin:
-    state = INITIAL
-    token = ''
-    for character in line:
-        if state == INITIAL and character == '0':
-            token += character
-            state = FOUND_0
-        elif state == FOUND_0 and (character.lower() == 'x'):
-            token += character
-            state = FOUND_X
-        elif state in [FOUND_X, FOUND_NUMBER] and (character.isnumeric() or character.lower() in ['a', 'b', 'c', 'd', 'e', 'f']):
-            token += character
-            state = FOUND_NUMBER
-        elif state == FOUND_NUMBER:
-            decimal = int(token, 16)
-            print(token, decimal)
-            token = ''
-            state = INITIAL
-        else:
-            token = ''
-            state = INITIAL
+    matches = hexPattern.findall(line)
+    for match in matches:
+        print(match, int(match, 16))
